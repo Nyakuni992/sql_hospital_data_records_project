45f76d2de323530a476d9033b5503503e6594a13
Nyakuni992 <nyanetah@gmail.com>
2026-05-29 13:00:32 +0300

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,15 @@
+-- Create Database 'Hospital_db'
+
+USE master;
+
+CREATE DATABASE Hospital_db
+
+--Create Schemas in the database
+
+USE Hospital_db;
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
